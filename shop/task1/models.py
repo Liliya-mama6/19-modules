3dd774441cfe2b4112adfb1e686f558e4325da4a
admin <vozdvizhenka@mail.ru>
2024-12-14 16:51:46 +0500

    all in one

--- shop/task1/models.py
+++ shop/task1/models.py
@@ -11,8 +11,10 @@ class Buyer(models.Model):
 
 class Game(models.Model):
     title = models.CharField(max_length=40)
-    cost = models.DecimalField(decimal_places=80, max_digits=99)
+    cost = models.DecimalField(decimal_places=0, max_digits=99)
     size = models.DecimalField(decimal_places=80, max_digits=99)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='games')
+    def __str__(self):
+        return self.title
--- shop/task1/views.py
+++ shop/task1/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+from .forms import User
+
+
+def registration(request):
+    info={}
+    global form
+    buyers = Buyer.objects.all()
+    users=[]
+    for i in buyers:
+        users.append(i.name)
+    if request.method == 'POST':
+        form = User(request.POST)
+        s=[]
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+            s.append(name)
+            s.append(age)
+            s.append(password)
+            s.append(repeat_password)
+        if s[0] in users:
+            return HttpResponse('This user yet was')
+        if s[2]!=s[3]:
+            return HttpResponse('wrong password')
+        Buyer.objects.create(name=s[0], balance=0, age=s[1])
+        return HttpResponse('all was successful')
+    else:
+        form = User()
+    return render(request, 'registration.html', {'form': form})
+
+def shop(request):
+    games = Game.objects.all()
+    context = {
+        'games': games
+    }
+    return render(request, 'shop.html', context)
+
+
+def main(request):
+    return render(request, 'main.html')
+
+
+def shopping_cart(request):
+    return render(request, 'shopping_cart.html')
